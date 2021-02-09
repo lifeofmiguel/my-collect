e5cd63a9a75275bae858af1ff6e73e025ede7232
Miguel Angel Morales <mmorales21500@live.com>
2021-02-09 09:02:07 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+    result = []
+    i = 0
+    while i < array.size
+      block_given? 
+      result << yield(array[i])
+      i += 1
+      
+    end
+  result
+  end
